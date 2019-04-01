c2ab71f34b4a19d81c951840ad8403f30cc698c7
Ryan Cox <rbc@Ryans-MacBook-Pro-3.local>
2019-04-01 14:20:12 -0400

    Updates

---
+++ precache-manifest.761dd6dde59841745db4c83baebce81d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d670e5868dbd777e5481acc3346cf4",
+    "url": "/story-cube-app/static/media/mountain.d9d670e5.svg"
+  },
+  {
+    "revision": "959fed9e64a0fb4402cc",
+    "url": "/story-cube-app/static/css/main.fa05b69f.chunk.css"
+  },
+  {
+    "revision": "b4ca18771ca21ffcdd27",
+    "url": "/story-cube-app/static/js/runtime~main.2ec7a89e.js"
+  },
+  {
+    "revision": "27d9103dec9b3cff935c0694a1b4d5b0",
+    "url": "/story-cube-app/static/media/wolf.27d9103d.svg"
+  },
+  {
+    "revision": "6d43bae7acdfd81e1d1d",
+    "url": "/story-cube-app/static/js/2.676b93aa.chunk.js"
+  },
+  {
+    "revision": "e70a547cb9a1c4466fbb61c544a37cff",
+    "url": "/story-cube-app/static/media/book.e70a547c.svg"
+  },
+  {
+    "revision": "3a7088ca9499cf91b305af74523cc5f4",
+    "url": "/story-cube-app/static/media/alien.3a7088ca.svg"
+  },
+  {
+    "revision": "88e9e3c6a7153a3854818b22d31ffe56",
+    "url": "/story-cube-app/static/media/bow-and-arrow.88e9e3c6.svg"
+  },
+  {
+    "revision": "f8d417fe8cab269bd7d21d4fabd58dca",
+    "url": "/story-cube-app/static/media/butterfly.f8d417fe.svg"
+  },
+  {
+    "revision": "3145236045ca3580bb7af175975970d7",
+    "url": "/story-cube-app/static/media/castle.31452360.svg"
+  },
+  {
+    "revision": "4b25d959a024b59758be3a9d1d3ea309",
+    "url": "/story-cube-app/static/media/dragon.4b25d959.svg"
+  },
+  {
+    "revision": "b80a7f7b66fbf20b7d90e9749ed2fb95",
+    "url": "/story-cube-app/static/media/girl.b80a7f7b.svg"
+  },
+  {
+    "revision": "959fed9e64a0fb4402cc",
+    "url": "/story-cube-app/static/js/main.c6fa8c49.chunk.js"
+  },
+  {
+    "revision": "a99523bb003d7cc712ef06153e981594",
+    "url": "/story-cube-app/static/media/ship.a99523bb.svg"
+  },
+  {
+    "revision": "4d833fe22f3a8c90fb695800f2f0d72e",
+    "url": "/story-cube-app/static/media/snake.4d833fe2.svg"
+  },
+  {
+    "revision": "85d137cf64699e5e04c7e964918ab090",
+    "url": "/story-cube-app/static/media/spaceship.85d137cf.svg"
+  },
+  {
+    "revision": "355ea86f73e9f1ba83b3cf3635cbcd98",
+    "url": "/story-cube-app/static/media/storm.355ea86f.svg"
+  },
+  {
+    "revision": "a5945f160bf8c0f41e2214e4a48b9200",
+    "url": "/story-cube-app/static/media/sword.a5945f16.svg"
+  },
+  {
+    "revision": "a6bfda0ee9710764449bad10590fcfd4",
+    "url": "/story-cube-app/static/media/turtle.a6bfda0e.svg"
+  },
+  {
+    "revision": "f689a6a67023cfa7917f3ecc394e0905",
+    "url": "/story-cube-app/static/media/unicorn.f689a6a6.svg"
+  },
+  {
+    "revision": "1cdcad6a522a2ca8c8f28c0e73efead0",
+    "url": "/story-cube-app/static/media/wand.1cdcad6a.svg"
+  },
+  {
+    "revision": "a7b28cd5175d86be8ca33b34f4c620e8",
+    "url": "/story-cube-app/static/media/witch.a7b28cd5.svg"
+  },
+  {
+    "revision": "825fff079a8dfcb253e641ec1728a6ac",
+    "url": "/story-cube-app/static/media/wizard.825fff07.svg"
+  },
+  {
+    "revision": "6d43bae7acdfd81e1d1d",
+    "url": "/story-cube-app/static/css/2.1061f6be.chunk.css"
+  },
+  {
+    "revision": "ba28a77f0d63fad367b64febcfe53895",
+    "url": "/story-cube-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89ba00060ff7973903d90e0662c2652e.js"
+  "/story-cube-app/precache-manifest.761dd6dde59841745db4c83baebce81d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/story-cube-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
