dfd3d1efb74ac9cb6db0cee2d070acc6a212368e
Ryan Cox <rbc@Ryans-MacBook-Pro-3.local>
2019-04-01 14:08:27 -0400

    Updates

---
+++ precache-manifest.89ba00060ff7973903d90e0662c2652e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d670e5868dbd777e5481acc3346cf4",
+    "url": "/static/media/mountain.d9d670e5.svg"
+  },
+  {
+    "revision": "2b1201b3e81378e620b7",
+    "url": "/static/css/main.fa05b69f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27d9103dec9b3cff935c0694a1b4d5b0",
+    "url": "/static/media/wolf.27d9103d.svg"
+  },
+  {
+    "revision": "6d43bae7acdfd81e1d1d",
+    "url": "/static/js/2.676b93aa.chunk.js"
+  },
+  {
+    "revision": "e70a547cb9a1c4466fbb61c544a37cff",
+    "url": "/static/media/book.e70a547c.svg"
+  },
+  {
+    "revision": "3a7088ca9499cf91b305af74523cc5f4",
+    "url": "/static/media/alien.3a7088ca.svg"
+  },
+  {
+    "revision": "88e9e3c6a7153a3854818b22d31ffe56",
+    "url": "/static/media/bow-and-arrow.88e9e3c6.svg"
+  },
+  {
+    "revision": "f8d417fe8cab269bd7d21d4fabd58dca",
+    "url": "/static/media/butterfly.f8d417fe.svg"
+  },
+  {
+    "revision": "3145236045ca3580bb7af175975970d7",
+    "url": "/static/media/castle.31452360.svg"
+  },
+  {
+    "revision": "4b25d959a024b59758be3a9d1d3ea309",
+    "url": "/static/media/dragon.4b25d959.svg"
+  },
+  {
+    "revision": "b80a7f7b66fbf20b7d90e9749ed2fb95",
+    "url": "/static/media/girl.b80a7f7b.svg"
+  },
+  {
+    "revision": "2b1201b3e81378e620b7",
+    "url": "/static/js/main.af0e1d12.chunk.js"
+  },
+  {
+    "revision": "a99523bb003d7cc712ef06153e981594",
+    "url": "/static/media/ship.a99523bb.svg"
+  },
+  {
+    "revision": "4d833fe22f3a8c90fb695800f2f0d72e",
+    "url": "/static/media/snake.4d833fe2.svg"
+  },
+  {
+    "revision": "85d137cf64699e5e04c7e964918ab090",
+    "url": "/static/media/spaceship.85d137cf.svg"
+  },
+  {
+    "revision": "355ea86f73e9f1ba83b3cf3635cbcd98",
+    "url": "/static/media/storm.355ea86f.svg"
+  },
+  {
+    "revision": "a5945f160bf8c0f41e2214e4a48b9200",
+    "url": "/static/media/sword.a5945f16.svg"
+  },
+  {
+    "revision": "a6bfda0ee9710764449bad10590fcfd4",
+    "url": "/static/media/turtle.a6bfda0e.svg"
+  },
+  {
+    "revision": "f689a6a67023cfa7917f3ecc394e0905",
+    "url": "/static/media/unicorn.f689a6a6.svg"
+  },
+  {
+    "revision": "1cdcad6a522a2ca8c8f28c0e73efead0",
+    "url": "/static/media/wand.1cdcad6a.svg"
+  },
+  {
+    "revision": "a7b28cd5175d86be8ca33b34f4c620e8",
+    "url": "/static/media/witch.a7b28cd5.svg"
+  },
+  {
+    "revision": "825fff079a8dfcb253e641ec1728a6ac",
+    "url": "/static/media/wizard.825fff07.svg"
+  },
+  {
+    "revision": "6d43bae7acdfd81e1d1d",
+    "url": "/static/css/2.1061f6be.chunk.css"
+  },
+  {
+    "revision": "66ddf35564c75bc31bbfab2ec101eea1",
+    "url": "/index.html"
+  }
+];
